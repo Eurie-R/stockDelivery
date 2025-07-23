24eac3539fd708153ad42b2cdc48f8fa4a32374f
Eurie-R <ramoy.uriel@gmail.com>
2025-07-24 00:29:52 +0800

    Created the supplier list

--- stockDelivery/supplierHub/urls.py
+++ stockDelivery/supplierHub/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.landingpage, name='landingpage'),
+    path('supplierlist/', views.supplierlist, name='supplierlist'),
 ]
 
 appname = 'supplierHub'
--- stockDelivery/supplierHub/views.py
+++ stockDelivery/supplierHub/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from .models import Supplier
 
 def landingpage(request):
     return render(request, 'landingpage.html')
+
+def supplierlist(request):
+    # Logic to retrieve and display the list of suppliers
+    suppliers = Supplier.objects.all()  # Assuming you have a Supplier model
+    ctx = {'suppliers': suppliers}
+    return render(request, 'supplierlist.html',ctx)
+
+def supplierdetail(request, supplier_id):
+    # Logic to retrieve and display details of a specific supplier
+    supplier = Supplier.objects.get(id=supplier_id)
+    ctx = {'supplier': supplier}
+    return render(request, 'supplierdetail.html', ctx)
